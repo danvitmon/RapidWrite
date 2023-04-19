9b8e25505adc5d724518879da28a8f1b26e80a07
Daniel Monastirsky <danvitmon@gmail.com>
2023-04-19 13:33:31 -0700

    finished

--- js/site.js
+++ js/site.js
@@ -121,8 +121,10 @@ function displayEventData(currentEvents) {
     tableRow.querySelector('[data-id="event"]').textContent = event.event;
     tableRow.querySelector('[data-id="city"]').textContent = event.city;
     tableRow.querySelector('[data-id="state"]').textContent = event.state;
-    tableRow.querySelector('[data-id="attendance"]').textContent = event.attendance;
+    tableRow.querySelector('[data-id="attendance"]').textContent = event.attendance.toLocaleString();
     tableRow.querySelector('[data-id="date"]').textContent = new Date(event.date).toLocaleDateString();
+
+    tableRow.querySelector('tr').setAttribute('data-event', event.id);
 
     eventTable.appendChild(tableRow);
   }
@@ -173,19 +175,32 @@ function getEventData() {
   let data = localStorage.getItem('jvSuperDogEventData');
 
   if (data == null) {
-    localStorage.setItem('jvSuperDogEventData', JSON.stringify(events));
+    let identifiedEvents = events.map((event) => {
+      event.id = generateId();
+      return event;
+    });
+
+    localStorage.setItem('jvSuperDogEventData', JSON.stringify(identifiedEvents));
+    data = localStorage.getItem('jvSuperDogEventData');
   }
 
-  let currentEvents = data == null ? events : JSON.parse(data);
+  let currentEvents = JSON.parse(data);
 
-  // if (data == null) {
-  //  currentEvents = events;
-  // } else {
-  //  currentEvents = JSON.parse(data);
-  // }
+  if (currentEvents.some(event => event.id == undefined)) {
 
+    currentEvents.forEach(event => event.id = generateId());
+
+    localStorage.setItem('jvSuperDogEventData', JSON.stringify(currentEvents));
+
+    // same as above if loop
+    // for (let i = 0; i < currentEvents.length; i++) {
+    //  let event = currentEvents[i];
+    //
+    //  event.id == generateId();
+    }
+    
   return currentEvents;
-}
+  }
 
 function viewFilteredEvents(dropdownItem) {
   let cityName = dropdownItem.getAttribute('data-string');
@@ -227,7 +242,7 @@ function saveNewEvent() {
   let attendance = parseInt(document.getElementById('newEventAttendance').value, 10);
 
   let dateValue = document.getElementById('newEventDate').value;
-  dateValue = new Date(dateValue);
+  dateValue = new Date(dateValue + ' 00:00');
 
   let date = dateValue.toLocaleDateString();
 
@@ -242,6 +257,7 @@ function saveNewEvent() {
     state: state,
     attendance: attendance,
     date: date,
+    id: generateId()
   };
 
   // add it to the array of current events
@@ -250,6 +266,114 @@ function saveNewEvent() {
 
   // then, save the array with the new event
   localStorage.setItem('jvSuperDogEventData', JSON.stringify(events));
+
+  buildDropDown();
+  document.getElementById('newEventForm').clear();
+}
+
+function generateId() {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+function editEvent(eventRow) {
+  let eventId = eventRow.getAttribute('data-event');
+
+  let currentEvents = getEventData();
+
+  let eventToEdit = currentEvents.find(event => event.id == eventId);
+
+  document.getElementById('editEventId').value = eventToEdit.id;
+  document.getElementById('editEventName').value = eventToEdit.event;
+  document.getElementById('editEventCity').value = eventToEdit.city;
+  document.getElementById('editEventAttendance').value = eventToEdit.attendance;
+
+  let eventDate = new Date(eventToEdit.date + ' 00:00');
+  let eventDateString = eventDate.toISOString();
+  let dateArray = eventDateString.split('T');
+  let formattedDate = dateArray[0];
+
+  
+  document.getElementById('editEventDate').value = formattedDate;
+
+  /*
+  let stateSelect = document.getElementById('newEventState');
+  let stateIndex = stateSelect.selectedIndex;
+  let state = stateSelect.options[stateIndex].text;
+  */
+
+  let editStateSelect = document.getElementById('editEventState');
+
+  let optionsArray = [...editStateSelect.options]
+
+  let index = optionsArray.findIndex(option => eventToEdit.state == option.text);
+  editStateSelect.selectedIndex = index // the index of the state for our event
+
+  /* for loop to find the option
+  for (let i = 0; i < editStateSelect.options.length; i++) {
+    let option = editStateSelect.option[i];
+
+    if (eventToEdit.state == option.text) {
+      editStateSelect.selectedIndex = i;
+    }
+  } */
+}
+
+function deleteEvent() {
+  let eventId = document.getElementById('editEventId').value;
+
+  // get the events in local storage as an array
+  let currentEvents = getEventData();
+  // filter out any event(s) with that eventId
+  let filteredEvents = currentEvents.filter(event => event.id != eventId);
+  // save that array to local storage
+  localStorage.setItem('jvSuperDogEventData', JSON.stringify(filteredEvents));
+
+  buildDropDown();
+}
+
+function updateEvent() {
+  let eventId = document.getElementById('editEventId').value;
+
+  // get the form input values
+  let name = document.getElementById('editEventName').value;
+  let city = document.getElementById('editEventCity').value;
+  let attendance = parseInt(document.getElementById('editEventAttendance').value, 10);
+
+  let dateValue = document.getElementById('editEventDate').value;
+  dateValue = new Date(dateValue);
+
+  let date = dateValue.toLocaleDateString();
+
+  let stateSelect = document.getElementById('editEventState');
+  let stateIndex = stateSelect.selectedIndex;
+  let state = stateSelect.options[stateIndex].text;
+
+  // create a new event object
+  let newEvent = {
+    event: name,
+    city: city,
+    state: state,
+    attendance: attendance,
+    date: date,
+    id: eventId
+  };
+
+  // get my events array
+  let currentEvents = getEventData();
+  // find the location of the OLD event with this ID
+  let index = -1;
+  for (let i = 0; i < currentEvents.length; i++) {
+    if (currentEvents[i].id == eventId) {
+      // replace that event with newEvent
+      currentEvents[i] = newEvent;
+      break;
+    }
+  }
+
+  // save it in local storage
+  localStorage.setItem('jvSuperDogEventData', JSON.stringify(currentEvents));
 
   buildDropDown();
 }
